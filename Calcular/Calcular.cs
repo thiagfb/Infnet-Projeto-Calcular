7b5b160138f002a3261897d49cd8d15f6c2d4591
thiagfb <thiagfb@hotmail.com>
2023-09-13 17:42:38 -0300

    Add project files.

---
+++ Calcular/Calcular.cs
@@ -0,0 +1,8 @@
+﻿namespace Calcular
+{
+    public class Calcular
+    {
+        public Double valor1 { get; set; }
+        public Double valor2 { get; set; }
+    }
+}
---
+++ Calcular/Controllers/CalcularController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calcular.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalcularController : ControllerBase
+    {
+        [HttpPut("Somar")]
+        public Double Somar([FromBody] Calcular a)
+        {
+            return a.valor1 + a.valor2;
+        }
+
+        [HttpPut("Subtrair")]
+        public Double Subtrair([FromBody] Calcular b)
+        {
+            return b.valor1 - b.valor2;
+        }
+    }
+}
