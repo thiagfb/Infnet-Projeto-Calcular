136ffea057a700f78adb8fa5841b0376040542e1
thiagfb <thiagfb@hotmail.com>
2023-09-19 19:48:33 -0300

    Implementando o HealthCheck e a página healthz

---
+++ Calcular/HealthCheck/HealthCheckRandom.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Calcular.HealthCheck
+{
+    public class HealthCheckRandom : IHealthCheck
+    {
+        private HttpClient _httpClient;
+        
+        public HealthCheckRandom(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClient = httpClientFactory.CreateClient();
+        }
+
+        async Task<HealthCheckResult> IHealthCheck.CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            String _apiUrl = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
+            HttpResponseMessage response;
+
+            try
+            {
+                response = await _httpClient.GetAsync(_apiUrl, cancellationToken);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // A resposta da API foi bem-sucedida, o que indica um estado de saúde "saudável".
+                    return HealthCheckResult.Healthy("API de câmbio está respondendo corretamente.");
+                    //response = await this._httpClient.GetAsync("http://httpbin.org/status/200", cancellationToken);
+                }
+                else
+                {
+                    // A resposta da API não foi bem-sucedida, o que indica um estado de saúde "não saudável".
+                    return HealthCheckResult.Unhealthy("API de câmbio retornou um status de erro: " + response.StatusCode);
+                    //response = await this._httpClient.GetAsync("http://httpbin.org/status/500", cancellationToken);
+                }
+
+                //return response.IsSuccessStatusCode ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy(description: "Deu ruim na API");
+            }
+            catch (Exception ex)
+            {
+                // Ocorreu uma exceção ao acessar a API, o que indica um estado de saúde "não saudável".
+                return HealthCheckResult.Unhealthy("Erro ao acessar a API de câmbio: " + ex.Message);
+            }
+        }
+    }
+}
--- Calcular/Program.cs
+++ Calcular/Program.cs
@@ -1,25 +1,55 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Calcular.HealthCheck;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//builder.Services.AddHealthChecks()
+//                .AddUrlGroup(new Uri("http://httpbin.org/status/200"), "Api Terceiro não autenticada")
+//                .AddUrlGroup(uri: new Uri("https://economia.awesomeapi.com.br/json/last/USD-BRL"), "Api Publica de cotação não autenticada")
+//                .AddCheck<HealthCheckRandom>(name: "Api Terceiro Autenticada");
+
+builder.Services.AddHealthChecks()
+                .AddCheck<HealthCheckRandom>(name: "Validando APIs");
+
+
+builder.Services.AddHealthChecksUI(s =>
+{
+    s.AddHealthCheckEndpoint("API-healthz", "https://localhost:7247/healthz");
+
+})
+.AddInMemoryStorage();
+
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseRouting()
+   .UseEndpoints(config =>
+   {
+       config.MapHealthChecks("/healthz", new HealthCheckOptions
+       {
+           Predicate = _ => true,
+           ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+       });
+
+       config.MapHealthChecksUI();
+   });
 
 app.Run();
